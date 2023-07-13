2129a031522fef5850e45c15e516abd45e7011b0
RussellAbraham <russell_a@live.com>
2023-07-12 17:56:27 -0700

    refactor textarea event and styles related to event

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,19 @@
+/* global $ */
 $(document).ready(function() {
-  
-  // todo : update to toglle css classes, disable the textarea or prevent submission of the form if character length is exceeded
-  $('form').on('input', 'textarea', function(event){
-
+  $('form').on('input', 'textarea', function(event) {
     const max = 140;
     const charCount = $(this).val().length;
-
-    let counting = max - charCount;
-
+    const counting = max - charCount;
     const counter = $(this).parent().find('.counter');
-
-    if(counting < 0) {
-      counter.css('color', 'red');
-    }
-
     counter.text(counting);
-    
-    if (counting > 0) {
-      counter.css('color', '#545454');
+    if (counting < 0) {
+      counter.addClass('char-limit');
+      $(this).closest('form').submit(function(e) {
+        e.preventDefault();
+      });
+    } else {
+      counter.removeClass('char-limit');
+      $(this).closest('form').unbind('submit');
     }
-
-    event.stopPropagation();
-
   });
-  
 });
