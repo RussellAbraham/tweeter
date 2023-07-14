4fa4184f38b415ca2e0e28c88994851b9e64126f
RussellAbraham <russell_a@live.com>
2023-07-13 21:02:06 -0700

    create renderTweets and createTweetElement functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,70 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function(tweets) {
+  const fragment = document.createDocumentFragment(); // Create a document fragment
+  // Loop through tweets
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet); // Create a tweet element
+    const article = document.createElement('article');
+    article.classList.add('article');
+    article.innerHTML = $tweet;
+    // Append the tweet element to the document fragment
+    fragment.appendChild(article); // Access the DOM element using $tweet[0]
+  }
+  $('.tweets-container').append(fragment); // Append the fragment to the tweets container
+};
+
+const createTweetElement = function (tweetData) {
+    const userTweets = `<header class="article-tweet-header">
+        <div class="article-tweet-header-profile">
+          <div class="tweet-header-icon">
+            <img src= ${tweetData.user.avatars}">
+          </div>  
+          <p>${tweetData.user.name}</p>
+        </div>
+        <div class="tweet-handle">${tweetData.user.handle}</div>
+        </header>
+        <p class="tweet-text">${tweetData.content.text}</p>
+      <footer class="article-tweet-footer">
+        <div class="tweet-post-date">${tweetData.created_at}</div>
+        <div class="tweet-footer-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-sharp fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>`
+
+    return userTweets;
+};
+
+$(document).ready(function() {  
+  renderTweets(data);
+})
