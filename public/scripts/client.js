a17fa9d9fa0b64ea3905d4cf7edfb56ccef3dcc2
RussellAbraham <russell_a@live.com>
2023-07-25 21:00:23 -0700

    Refactor client and styles

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -20,82 +20,86 @@ const renderTweets = function (tweets) {
     article.classList.add('article');
     article.innerHTML = $tweet;
     // Append the tweet element to the document fragment
-    fragment.appendChild(article); // Access the DOM element using $tweet[0]
+    fragment.appendChild(article);
   }
   $('.tweets-container').append(fragment); // Append the fragment to the tweets container
 };
 
+// tweet template
 const createTweetElement = function (tweetData) {
-  const userTweets = `<header class="article-tweet-header">
-        <div class="article-tweet-header-profile">
-          <div class="tweet-header-icon">
-            <img src="${escapeHTML(tweetData.user.avatars)}">
-          </div>  
-          <p>${escapeHTML(tweetData.user.name)}</p>
-        </div>
-        <div class="tweet-handle">${escapeHTML(tweetData.user.handle)}</div>
-        </header>
-        <p class="tweet-text">${escapeHTML(tweetData.content.text)}</p>
-      <footer class="article-tweet-footer">
-        <div class="tweet-post-date">${escapeHTML(timeago.format(tweetData.created_at))}</div>
-        <div class="tweet-footer-icons">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-sharp fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </div>
-      </footer>`;
+  const userTweets = `
+    <header class="article-tweet-header">
+      <div class="article-tweet-header-profile">
+        <div class="tweet-header-icon">
+          <img src="${escapeHTML(tweetData.user.avatars)}">
+        </div>  
+        <p>${escapeHTML(tweetData.user.name)}</p>
+      </div>
+      <div class="tweet-handle">${escapeHTML(tweetData.user.handle)}</div>
+    </header>
+    <p class="tweet-text">${escapeHTML(tweetData.content.text)}</p>
+    <footer class="article-tweet-footer">
+      <div class="tweet-post-date">${escapeHTML(timeago.format(tweetData.created_at))}</div>
+      <div class="tweet-footer-icons">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-sharp fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  `;
 
   return userTweets;
 };
 
 $(document).ready(function () {
-
   $('form').submit(function (event) {
-
     event.preventDefault();
 
     const tweetContent = $("#tweet-text").val();
     const tweetError = $("#new-tweet-error-message");
 
-
-    if (tweetContent.length) {
-      tweetError.hide().text();
+    // Check for empty tweet content
+    if (!tweetContent.trim()) {
+      tweetError.text('Your have no content to submit').slideDown(500);
+      return;
     }
 
     const maxTweetLength = 140;
     if (tweetContent.length > maxTweetLength) {
-      //alert("Tweet content is too long. Maximum " + maxTweetLength + " characters allowed.");
-      tweetError.text("You've exceed the characer limit!").slideDown(500);
+      tweetError.text("You've exceeded the character limit!").slideDown(500);
       return; // Do not proceed with form submission
     }
 
-    if (!tweetContent.length) {
-      tweetError.text('Your have no content to submit').slideDown(500);
-      return;
-    } else {
-      $.ajax({
-        type: "POST",
-        url: "/tweets",
-        data: $(this).serialize(),
-        success: function (data) {
-          loadTweets();
-        }
-      })
-    }
+    // Clear error message when content is valid
+    tweetError.hide().text();
 
+    // Proceed with form submission using AJAX
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: $(this).serialize(),
+      success: function (data) {
+        loadTweets(); // Load tweets again after successful submission
+        $("#tweet-text").val("");
+        $(".counter").text(140);
+      },
+      error: function (error) {
+        console.log("Error posting tweet:", error);
+      }
+    });
   });
 
-
   const loadTweets = function () {
-    $.getJSON({
-      url: "/tweets",
-      success: function (data) {
+    // Use $.get for simplicity when just fetching JSON
+    $.get("/tweets")
+      .done(function (data) {
         $("#tweets-container").empty();
         renderTweets(data);
-      }
-    })
+      })
+      .fail(function (error) {
+        console.log("Error loading tweets:", error);
+      });
   };
 
   loadTweets();
-
-})
+});
